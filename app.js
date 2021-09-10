5dc6326a11c8eaed8189af4408597b6a1aaabd7a
Juan Sebasian Soto Martinez <atlasjuanse12@gmail.com>
2021-09-10 02:34:36 -0500

    "agregando rutas basicas"

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.json({response:'success'});
+});
+
+app.listen(3000, () =>{
+    console.log('tu puta madre');
+});
