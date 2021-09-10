1895df195209d6a24ad251540236246b7ce988a1
Juan Sebasian Soto Martinez <atlasjuanse12@gmail.com>
2021-09-10 02:53:41 -0500

    "Conexion a la base de datos"

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+
+mongoose.connect('mongodb://guillermo:123@Localhost:27017/praacticasCRUD', {useNewUrlparser: true, useUnifiedTopology: true});
+
+const connection=mongoose.connection;
+
+connection.once('open',()=>{
+    console.log('conexion exitosa');
+});
+
+connection.on('error', (err)=>{
+    console.log('error en la conexion a la BD:',err);
+});
+
 app.get('/', (req, res) =>{
     res.json({response:'success'});
 });
